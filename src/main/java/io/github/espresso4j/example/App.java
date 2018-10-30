e0319f92c1072c8602eb93515ffbe25a51291718
Ning Sun <sunng@protonmail.com>
2018-10-30 23:13:46 +0800

    (feat) update example, adding latte

--- src/main/java/io/github/espresso4j/example/App.java
+++ src/main/java/io/github/espresso4j/example/App.java
@@ -1,16 +1,24 @@
 package io.github.espresso4j.example;
 
 import io.github.espresso4j.espresso.*;
+import io.github.espresso4j.latte.Latte;
 import io.github.espresso4j.jettino.JettinoAdapter;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         JettinoAdapter ja = new JettinoAdapter();
-        ja.start((request) -> new Response()
-                 .setStatus(200)
-                 .addHeader("Content-Type", "text/html")
-                 .setBody(String.format("<h1>It works</h1><p>%s</p>", request)));
+
+        Espresso latte = Latte.sync()
+            .on("/", (req) -> Response.of(200)
+                .header("Content-Type", "text/html")
+                .body(String.format("<h1>It works</h1><p>%s</p>", req)))
+            .on("/user/:name", (req) -> Response.of(200)
+                .header("Content-Type", "text/plain")
+                .body(String.format("Hello, %s", Latte.getExtension(req).get("name"))))
+            .buildSync();
+
+        ja.start(latte);
     }
 
 }
