d6772854d691698298df77fe490706de17bdeb24
Ning Sun <sunng@protonmail.com>
2018-11-10 17:56:19 +0800

    (feat) added sugar example

--- src/main/java/io/github/espresso4j/example/App.java
+++ src/main/java/io/github/espresso4j/example/App.java
@@ -2,6 +2,7 @@ package io.github.espresso4j.example;
 
 import io.github.espresso4j.espresso.*;
 import io.github.espresso4j.latte.Latte;
+import io.github.espresso4j.sugar.Sugar;
 import io.github.espresso4j.jettino.JettinoAdapter;
 
 public class App {
@@ -9,16 +10,21 @@ public class App {
     public static void main(String[] args) throws Exception {
         JettinoAdapter ja = new JettinoAdapter();
 
-        Espresso latte = Latte.sync()
+        Espresso latte = Latte.by(Espresso.class)
             .on("/", (req) -> Response.of(200)
                 .header("Content-Type", "text/html")
                 .body(String.format("<h1>It works</h1><p>%s</p>", req)))
             .on("/user/:name", (req) -> Response.of(200)
                 .header("Content-Type", "text/plain")
                 .body(String.format("Hello, %s", Latte.extension(req).get("name"))))
-            .buildSync();
+            .on("/user/:name/update", (req) -> Response.of(200)
+                .header("Content-Type", "text/plain")
+                .body(String.format("Updating user %s to age %s",
+                                    Latte.extension(req).get("name"),
+                                    Sugar.extension(req).get("age"))))
+            .intoEspresso();
 
-        ja.start(latte);
+        ja.start(new Sugar(latte));
     }
 
 }
